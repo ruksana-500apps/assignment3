d84295bdc125edc1b9f085fd3e1e998fb99b702a
ruksana-500apps <102018993+ruksana-500apps@users.noreply.github.com>
2022-04-04 19:41:25 +0530

    ok

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
+
 import App from './App.vue'
+
+import router from './router'
+
+import { BootstrapVue } from "bootstrap-vue";
+
+import { IconsPlugin } from 'bootstrap-vue'
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
+Vue.use(BootstrapVue);
+
+Vue.use(IconsPlugin);
+
+
+let vueRoute=new Vue({
+
+router,
+
+render: h => h(App),
+
 }).$mount('#app')
+
+export default vueRoute
+
+
+
+
