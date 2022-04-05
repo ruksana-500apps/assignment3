5eb96b96d0681f91fc960cedaed358692da8ccf5
ruksana-500apps <102018993+ruksana-500apps@users.noreply.github.com>
2022-04-05 11:59:28 +0530

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -2,102 +2,64 @@ import Vue from 'vue'
 
 import Router from 'vue-router'
 
-//import HelloWorld from '../components/HelloWorld'
+
+import ques1 from '../components/question1.vue'
+import ques2 from '../components/question2.vue'
+import ques3 from '../components/question3.vue'
+import ques5 from '../components/question5.vue'
+import ques6 from '../components/question6.vue'
+import ques7 from '../components/question7.vue'
+import ques8 from  '../components/question8.vue'
+import ques10 from '../components/question10.vue'
 
 Vue.use(Router)
 
 const routes=[{
 
-path:'/ques9',
-
-name:"q5",
-
-component:ques9
-
-},
-
-{
-
-path:'/ques5',
-
-name:"traindata",
-
-component:ques5
-
-},
-
-{
-
-path:'/ques6',
-
-name:"map",
-
-component:ques6
-
-},
-
-{
-
-path:'/ques7',
-
-name:"foreach",
-component:ques7
-
-},
-
-{
-
-path:'/ques8',
-
-name:"indexs",
-
-component:ques8
-
-},
-
-{
-
 path:'/ques1',
 
-name:"namechange",
+name:"ques1",
 
 component:ques1
 
 },
-
 {
-
-path:'/ques2',
-
-name:"stringlit",
-
-component:ques2
-
-},
-
-{
-
-path:'/ques3',
-
-name:"spread",
-
-component:ques3
-
+    path:'/ques2',
+    name:"ques2",
+    component:ques2
 },
 {
-
+    path:'/ques3',
+    name:"ques3",
+    component:ques3
+},
+{
+    path:'/ques5',
+    name:"ques5",
+    component:ques5
+},
+{
+    path:'/ques6',
+    name:"ques6",
+    component:ques6
+},
+{
+    path:'/ques7',
+    name:"ques7",
+    component:ques7
+},
+{
     path:'/ques8',
-    
-    name:"indexs",
-    
+    name:"ques8",
     component:ques8
-    
-    }
+},
 
+{
+    path:'/ques10',
+    name:"ques10",
+    component:ques10
+}
 ]
-
 let router=new Router({routes})
 
 export default router
-
-
